8889ef7ebf9fe662f099a07e18e1944b41f400b4
Ivan Parkhomenko <ippeng54@gmail.com>
2023-04-22 22:54:06 +0300

    homework 4

--- src/video.py
+++ src/video.py
@@ -1,5 +1,6 @@
 import json
 import os
+# необходимо установить через: pip install google-api-python-client
 from googleapiclient.discovery import build
 
 
@@ -10,3 +11,11 @@ class Video:
 
     def __init__(self, id_video):
         self.id_video = id_video
+        video_response = self.youtube.videos().list(part='snippet,statistics,contentDetails,topicDetails',
+                                               id=id_video
+                                               ).execute()
+        # printj(video_response)
+        self.video_title: str = video_response['items'][0]['snippet']['title']
+        self.view_count: int = video_response['items'][0]['statistics']['viewCount']
+        self.like_count: int = video_response['items'][0]['statistics']['likeCount']
+        self.url: str = video_response['items'][0]['snippet']['url']
