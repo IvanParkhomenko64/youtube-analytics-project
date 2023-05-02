47c985c94dac3863dbbbc84b55f2f47c90ea0880
ivan <ippeng54@gmail.com>
2023-05-02 14:05:18 +0400

    homework 6 edit commit

--- homework-6/main.py
+++ homework-6/main.py
@@ -1,6 +1,7 @@
 from src.video import Video
 
 if __name__ == '__main__':
+    """Update Project"""
     broken_video = Video('broken_video_id')
     assert broken_video.video_title is None
     assert broken_video.like_count is None
